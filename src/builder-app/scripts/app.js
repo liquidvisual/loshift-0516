5eb5714b8beb7741adf5dc5b46521dd5967fb216
Mitchell Warren <hello@liquidvisual.com>
2016-07-16 11:19:49 +1000

    Builder app update - login

--- src/builder-app/scripts/app.js
+++ src/builder-app/scripts/app.js
@@ -4,123 +4,118 @@
 //-----------------------------------------------------------------
 // Variables
 //-----------------------------------------------------------------
-
-// localStorage.setItem("loggedIn", "");
-var loggedIn = localStorage.getItem("loggedIn");
-
 //-----------------------------------------------------------------
-// Document Ready
+// DOC READY
 //-----------------------------------------------------------------
 
-if (loggedIn) {
-	loadIncludes();
-} else {
-
-	// Login
-	assembleView();
-	$('.lv-page').removeClass('is-loading');
-}
-
-//-----------------------------------------------------------------
-// Pixel Gradient
-//-----------------------------------------------------------------
-
-function applyGradientHeader() {
-	$("h1.subheader, h2.subheader, h3.subheader").pxgradient({
-	  step: 10, // Step Color. The smaller the number, the greater the load. Default: 10
-	  colors: ["#F12D66","#20B5C3"],
-	  dir: "x"
-	});
-}
-
-$(window).load(function() {
-	applyGradientHeader();
+$(function(){
+	authenticate(); // ===> FIRST POINT OF ENTRY
 });
 
 //-----------------------------------------------------------------
+// AUTHENTICATE
 //
+// 1. Authenticate - if logged in, pull in includes of app frame
+// otherwise load in main view only.
+// 2. Init
+// 3. Hash Change will trigger Init
+//-----------------------------------------------------------------
+
+function authenticate() {
+	if (loggedIn) {
+		loadIncludes();
+	} else {
+		loadMainView();
+	}
+}
+
+//-----------------------------------------------------------------
+// INITIALISE
 //-----------------------------------------------------------------
 
 function init(){
 	setToolTab();
-	setSecondLevel();
+	setlevel2();
 	initToolButtons();
-	// hljs.initHighlightingOnLoad();
-	assembleView();
-	$('.lv-page').removeClass('is-loading');
-
-	// alert('fuckyoa');
+	loadMainView();
 }
 
-//-----------------------------------------------------------------
-// ON HASH CHANGE
-//-----------------------------------------------------------------
-
-$(window).on('hashchange', function() {
-    setToolTab();
-    setSecondLevel();
-    assembleView();
-});
+$(window).on('hashchange', init);
 
 //-----------------------------------------------------------------
-// loadInclude - caching?
+// LOAD INCLUDES (Caching?)
 //-----------------------------------------------------------------
 
-function loadIncludes(path) {
-	// var totalIncludes = $('[data-include').length;
+function loadIncludes() {
+	var $includes = $('[data-include]');
+	var totalIncludes = $includes.length;
+	var includesLoaded = 0;
 
-	$('[data-include]').each(function(){
+	// When all includes have loaded, safe to INIT
+	function registerInclude() {
+		includesLoaded ++;
+		includesLoaded == totalIncludes ? init() : null;
+	}
+
+	// Register include after loading
+	$includes.each(function(){
 		var path = $(this).attr('data-include');
-		$(this).load(path, init);
+		$(this).load(path, registerInclude);
 	})
 }
 
 //-----------------------------------------------------------------
-// loadView
+// LOAD MAIN VIEW
 //-----------------------------------------------------------------
 
-function assembleView() {
-
-	var firstLevel = getHashBang().firstLevel;
-	var secondLevel = getHashBang().secondLevel;
-	var path = '/builder-app/views/'+firstLevel+'/'+secondLevel+'.html';
+function loadMainView() {
+	var $mainView = $('[data-view]');
+	var level1 = getHashBang().level1;
+	var level2 = getHashBang().level2;
+	var level3 = getHashBang().level3;
+	var path = '/builder-app/views/'+level1+'/'+level2+'.html';
 
 	if (!loggedIn) {
-		$('[data-view]').load('/builder-app/views/login.html');
+		$mainView.load('/builder-app/views/login.html', onLoadComplete);
 		return;
 	}
 
 	// If the second level exists in the hashbang
-	if (secondLevel) {
-	    $('[data-view]').load(path, function(response, status, xhr) {
-			if (status == "error") {
-				// 404
-				$('[data-view]').load('/builder-app/views/404.html');
+	if (level2) {
+
+	    $mainView.load(path, function(response, status, xhr) {
+	    	onLoadComplete();
+
+	    	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	    	// SUCCESS
+	    	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	    	if (status == 'success') {
+	    		// applyGradientHeader();
+	    	}
+	    	//=========================================
+	    	// 404
+	    	//=========================================
+
+			else if (status == "error") {
+				$(this).load('/builder-app/views/404.html');
+				console.log('Failed to load. Attempting to load 404 page.');
 			}
 		});
 	}
 
-    // Trigger AppMode if 'Preview' is triggered
-    if (getHashBang().firstLevel !== 'preview' ) {
+	//=========================================
+	// Trigger "AppMode" if 'Preview' selected
+	//=========================================
+
+    if (getHashBang().level1 !== 'preview' ) {
     	$('body').addClass('menu-is-open');
     }
+    onLoadComplete();
 }
 
 //-----------------------------------------------------------------
-// Get Hash Bang
-//-----------------------------------------------------------------
-
-function getHashBang() {
-	var hashBang_arr = window.location.hash.split('/');
-	var hashBang = {};
-		hashBang.firstLevel  = hashBang_arr[1];
-		hashBang.secondLevel = hashBang_arr[2];
-		hashBang.thirdLevel  = hashBang_arr[3];
-		return hashBang;
-}
-
-//-----------------------------------------------------------------
-// Init Tool Buttons
+// INIT TOOL BUTTONS
 //-----------------------------------------------------------------
 
 function initToolButtons() {
@@ -128,27 +123,16 @@ function initToolButtons() {
 	// Menu Button
 	$('.lv-tool-menu-btn').bind('click', function(e){
 		e.preventDefault();
+		console.log('Menu button is triggering.');
 		$('body').toggleClass('menu-is-open');
-
-
 		// var isAnimating = $(".lv-context-menu").is(':animated');
-
-
-
-		// var body = $('body');
-
-		// if (body.hasClass('menu-is-open')) {
-		// 	body.removeClass('menu-is-open');
-		// } else {
-		// 	body.addClass('menu-is-open');
-		// }
 	});
 
 	// come back here
 }
 
 //-----------------------------------------------------------------
-// Set Tool Tab
+// SET TOOL TAB (Vertical)
 //-----------------------------------------------------------------
 
 function setToolTab() {
@@ -157,45 +141,98 @@ function setToolTab() {
 		toolTabContent.attr('hidden', true);
 		$('[data-tool-tab]').removeClass('active');
 
-		// loop all tabs
+		// loop all tab content
 		toolTabContent.each(function(){
 			var $this = $(this);
 			var stringId = $this.attr('data-tool-tab-content');
 
-			// Choose Tab based on Hashbang
-			if (stringId == getHashBang().firstLevel) {
+			// Choose Tab based on URL Hashbang
+			if (stringId == getHashBang().level1) {
 				$this.removeAttr('hidden');
-				toolTabTitle.text(stringId);
-				applyGradientHeader();
 				$('[data-tool-tab="'+stringId+'"]').addClass('active');
+				// toolTabTitle.text(stringId);
+
+				setTimeout(function() {
+					console.log('YES');
+
+					$("[data-tool-tab-title]").pxgradient({
+					  step: 10, // Step Color. The smaller the number, the greater the load. Default: 10
+					  colors: ["#F12D66","#20B5C3"],
+					  dir: "x"
+					});
+
+				applyGradientHeader();
+				}, 1000);
 			}
 		});
+
+		// toolTabTitle.pxgradient({ step: 10, colors: ["#F12D66","#20B5C3"], dir: "x" });
 }
 
 //-----------------------------------------------------------------
-// Set Second Level
+// ON LOAD COMPLETE
 //-----------------------------------------------------------------
 
-function setSecondLevel() {
-	var secondLevel = getHashBang().secondLevel;
+function onLoadComplete() {
+	$('.lv-page').removeClass('is-loading');
+}
+
+//-----------------------------------------------------------------
+// SET LEVEL 2
+//-----------------------------------------------------------------
+
+function setlevel2() {
+	var level2 = getHashBang().level2;
 	$('.lv-context-menu-panel a').parent().removeClass('active');
 
-	// If the second level exists in the hashbang
-	if (secondLevel) {
-		$('.lv-context-menu-panel a[href*="'+secondLevel+'"]').parent().addClass('active');
+	// If second level exists in the hashbang
+	if (level2) {
+		$('.lv-context-menu-panel a[href*="'+level2+'"]').parent().addClass('active');
 	}
 }
 
 //-----------------------------------------------------------------
+// UTILITY: Get Hashbang
+//-----------------------------------------------------------------
+
+function getHashBang() {
+	var hashBang_arr = window.location.hash.split('/');
+	var hashBang = {};
+		hashBang.level1  = hashBang_arr[1];
+		hashBang.level2  = hashBang_arr[2];
+		hashBang.level3  = hashBang_arr[3];
+		hashBang.level4  = hashBang_arr[4];
+		hashBang.level5  = hashBang_arr[5];
+		hashBang.level6  = hashBang_arr[6];
+		hashBang.level7  = hashBang_arr[7];
+		hashBang.level8  = hashBang_arr[8];
+		hashBang.level9  = hashBang_arr[9];
+		return hashBang;
+}
+
+//-----------------------------------------------------------------
+// Pixel Gradient
+//-----------------------------------------------------------------
+
+function applyGradientHeader() {
+
+	$("h1.subheader, h2.subheader, h3.subheader").pxgradient({
+	  step: 10, // Step Color. The smaller the number, the greater the load. Default: 10
+	  colors: ["#F12D66","#20B5C3"],
+	  dir: "x"
+	});
+}
+
+// $(window).load(function() {
+// 	applyGradientHeader();
+// });
+
+//-----------------------------------------------------------------
 //
 //-----------------------------------------------------------------
 
-
-
-
 //-----------------------------------------------------------------
 //
 //-----------------------------------------------------------------
-
 
 
--- src/builder-app/scripts/login.js
+++ src/builder-app/scripts/login.js
@@ -1,28 +1,49 @@
 /*
-    LOGIN SCRIPTS - Last updated: 17.08.15
+    LOGIN SCRIPTS - Last updated: 15.07.16
 */
 //-----------------------------------------------------------------
 // Variables
 //-----------------------------------------------------------------
-//-----------------------------------------------------------------
-//
-//-----------------------------------------------------------------
 
-// function getLogin() {
-//     return localStorage.getItem("loggedIn");
-// }
-
-// function setLogin(boolean) {
-// 	if (typeof(Storage) != "undefined") {
-// 		localStorage.setItem("loggedIn", boolean);
-// 	} else {
-// 		alert('Local Storage is missing!');
-// 	}
-// }
+var loggedIn = localStorage.getItem("loggedIn");
 
 //-----------------------------------------------------------------
 //
 //-----------------------------------------------------------------
 
+$(window).load(function() {
 
+	$('[data-login]').click(function(e){
+		e.preventDefault();
+		setLogin(true);
 
+		// loadIncludes();	// start process
+		// window.location.replace('#/overview/welcome/');
+		// $(window).trigger('hashchange'); // wtf
+		location.reload();
+	});
+});
+
+//-----------------------------------------------------------------
+// Get Login
+//-----------------------------------------------------------------
+
+function getLogin() {
+    return localStorage.getItem("loggedIn");
+}
+
+//-----------------------------------------------------------------
+// Set Login
+//-----------------------------------------------------------------
+
+function setLogin(value) {
+	if (typeof(Storage) != "undefined") {
+		localStorage.setItem("loggedIn", value);
+	} else {
+		console.log('Oops! Local Storage not supported.');
+	}
+}
+
+//-----------------------------------------------------------------
+//
+//-----------------------------------------------------------------
